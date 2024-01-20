07f693da42302a6aefb06d0480470be747642089
sujan khatri <sujansince2059@gmail.com>
2024-01-20 15:53:22 +0545

    main app comp 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,65 @@
+import { useState } from "react";
 import "./App.css";
+import { InputBox } from "./Components";
+import { useCurrencyinfo } from "./hooks";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmt, setConvertedamt] = useState(0);
+
+  const currencyinfo = useCurrencyinfo(from);
+
+  const currencyOption = Object.keys(currencyinfo);
+  // console.log(currencyOption);
+
+  function swap() {
+    setFrom(to);
+    setTo(from);
+    setConvertedamt(amount);
+    setAmount(convertedAmt);
+  }
+
+  setConvertedamt(amount * currencyinfo[to]);
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/534216/pexels-photo-534216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox label="From" />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <InputBox label="To" />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
